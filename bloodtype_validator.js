2dd6efc6b2160c03cbbd47381dc66bc71cb12f8f
akamaozu <uzo@designbymobius.ca>
2016-05-31 16:14:14 -0400

    added callback support

--- bloodtype_validator.js
+++ bloodtype_validator.js
@@ -1,10 +1,29 @@
-module.exports = function is_valid_bloodtype( bloodtype, strict_comparison ){
+module.exports = function is_valid_bloodtype( bloodtype, strict_comparison, callback ){
 
-  if( !bloodtype || typeof bloodtype !== 'string' ) return false;
+  // if shorthand function arguments, determine if second argument is strict comparison flag or callback 
+    if( arguments.length == 2 ){
 
-  var bloodtype_validation_regex = /^(a?b?|o)([+-])?$/i;
-  
-  if( strict_comparison ) bloodtype_validation_regex = /^(a?b?|o)([+-])$/i;  
+      if( typeof strict_comparison === 'function' ){
 
-  return bloodtype_validation_regex.test( bloodtype.trim() );
+        callback = strict_comparison;
+        strict_comparison = false;
+      } 
+    }
+
+  // ensure bloodtype is given and is a string
+    if( !bloodtype || typeof bloodtype !== 'string' ){
+
+      if( callback ) return callback( new Error( 'bloodtype to check is required and must be a string' ) );
+
+      else return false;
+    }
+
+  var bloodtype_validation_regex = strict_comparison ? /^(a?b?|o)([+-])?$/i : /^(a?b?|o)([+-])$/i,
+      is_valid = false;
+
+  is_valid = bloodtype_validation_regex.test( bloodtype.trim() );
+
+  if( callback ) return callback( null, is_valid );  
+
+  else return is_valid;
 } 
